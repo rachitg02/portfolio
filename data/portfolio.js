7360aa34d193b5ff2d1604d70413bec6d0773641
Rachit <rachitgup02@gmail.com>
2022-11-23 10:14:19 +0530

    Portfolio update

--- data/portfolio.js
+++ data/portfolio.js
@@ -8,13 +8,13 @@
        {
         title:'Medfile',
         imgUrl:'/assets/medfile.png',
-        stack: ['Next.js','JavaScript','Firebase', 'Tailwind CSS'],
+        stack: ['Next.js', 'TypeScript', 'Tailwind CSS', 'Solidity', 'Hardhat', 'Ethers.js'],
         link:'https://medfile.vercel.app/',
     },
        {
         title:'Postit',
         imgUrl:'/assets/postit.png',
-        stack: ['Next.js','JavaScript','Firebase', 'Tailwind CSS'],
+        stack: ['Next.js','TypeScript','Firebase', 'Chakra UI','Recoil'],
         link:'https://github.com/rachitg02/postit',
     },
  ]
